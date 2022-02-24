2f048e7a3c2cd0a05f37d93a4fc5608060a1c764
TalvinderJohal <68028244+TalvinderJohal@users.noreply.github.com>
2022-02-24 09:13:54 +0000

    updated skeleton

--- model.py
+++ model.py
@@ -9,3 +9,14 @@ class Classify(nn.Module):
         self.hidden1 = nn.Linear(20,30)
         self.output  = nn.Linear(30,outdim)
         self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        input = self.in_lier(x)
+        ac1 = self.sigmoid(input)
+        input2 = self.hidden0(ac1)
+        ac2 = self.sigmoid(input2)
+        input3 = self.hidden1(ac2)
+        ac3 = self.sigmoid(input3)
+        output = self.output(ac3)
+        return output
+
